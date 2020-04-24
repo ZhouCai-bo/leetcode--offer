225d856580c2cf4c48bebf04d69227f492d44b83
周 <50270240+ZhouCai-bo@users.noreply.github.com>
2020-04-24 11:43:44 +0800

    Create 面试题52. 两个链表的第一个公共节点 双指针.cpp

---
+++ 面试题52. 两个链表的第一个公共节点 双指针.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+	ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+		auto p1 = headA;
+		auto p2 = headB;
+		while (p1 != p2 && (p1 || p2))
+		{
+			p1 = !p1 ? headB : p1->next;
+			p2 = !p2 ? headA : p2->next;
+		}
+		return p1;
+	}
+};
